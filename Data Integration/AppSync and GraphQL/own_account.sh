73c7c8b4917a6de1ddbbac0539359f86b1cd826b
shubha262001 <90273666+shubha262001@users.noreply.github.com>
2024-02-01 20:24:14 +0530

    Update own_account.sh

--- Data Integration/AppSync and GraphQL/own_account.sh
+++ Data Integration/AppSync and GraphQL/own_account.sh
@@ -12,30 +12,30 @@ aws cloudformation create-stack \
   --stack-name CodeCommitsk \
   --template-body file://codecommit.yaml \
   --parameters \
-      ParameterKey=FrontendRepoName,ParameterValue=AnyCompanyReads-frontend \
-      ParameterKey=BackendRepoName,ParameterValue=AnyCompanyReads-backend \
+      ParameterKey=FrontendRepoNamesk,ParameterValue=AnyCompanyReads-frontendsk \
+      ParameterKey=BackendRepoName,ParameterValue=AnyCompanyReads-backendsk \
       ParameterKey=S3CodeBucket,ParameterValue='' \
       ParameterKey=S3FrontendCodeKey,ParameterValue='' \
       ParameterKey=S3BackendCodeKey,ParameterValue=''
 
 echo 'Initializing git'
-git config --global user.name "Workshop User"
-git config --global user.email workshop.user@amazon.com
+git config --global user.name "shubha262001"
+git config --global user.email shubha2001shub@gmail.com
 git config --global credential.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
 
 echo 'Waiting for CodeCommit CloudFormation stack to complete'
-aws cloudformation wait stack-create-complete --stack-name CodeCommit
+aws cloudformation wait stack-create-complete --stack-name CodeCommitsk
 echo 'CodeCommit stack complete'
 
 echo 'Cloning Repository'
-git clone https://git-codecommit.$AWS_REGION.amazonaws.com/v1/repos/AnyCompanyReads-frontend
+git clone https://git-codecommit.$AWS_REGION.amazonaws.com/v1/repos/AnyCompanyReads-frontendsk
 
 echo 'Extracting AppSync Frontend code from zip file, overwriting any existing files'
-unzip -o appsync-frontend-starter.zip -d AnyCompanyReads-frontend
+unzip -o appsync-frontend-starter.zip -d AnyCompanyReads-frontendsk
 
 echo 'Uploading code to repository'
-cd AnyCompanyReads-frontend
+cd AnyCompanyReads-frontendsk
 git checkout -b main
 git add .
 git commit -m "Initial Commit"
@@ -43,13 +43,13 @@ git push --set-upstream origin main
 cd ..
 
 echo 'Cloning Repository'
-git clone https://git-codecommit.$AWS_REGION.amazonaws.com/v1/repos/AnyCompanyReads-backend
+git clone https://git-codecommit.$AWS_REGION.amazonaws.com/v1/repos/AnyCompanyReads-backendsk
 
 echo 'Extracting AppSync Backend code from zip file, overwriting any existing files'
-unzip -o appsync-backend-starter.zip -d AnyCompanyReads-backend
+unzip -o appsync-backend-starter.zip -d AnyCompanyReads-backendsk
 
 echo 'Uploading code to repository'
-cd AnyCompanyReads-backend
+cd AnyCompanyReads-backendsk
 git checkout -b main
 git add .
 git commit -m "Initial Commit"
@@ -58,15 +58,15 @@ cd ..
 
 echo 'Starting Cloud9 CloudFormation stack'
 aws cloudformation create-stack \
-  --stack-name Cloud9 \
+  --stack-name Cloud9sk \
   --template-body file://cloud9.yaml \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameters \
       ParameterKey=InstanceOwner,ParameterValue='' \
-      ParameterKey=InstanceName,ParameterValue=appsync-workshop 
+      ParameterKey=InstanceName,ParameterValue=appsync-workshop-sk 
 
 echo 'Waiting for Cloud9 stack to complete. Can take around 5 minutes, polling every 30 seconds'
-aws cloudformation wait stack-create-complete --stack-name Cloud9
-echo 'Cloud9 stack complete'
+aws cloudformation wait stack-create-complete --stack-name Cloud9sk
+echo 'Cloud9sk stack complete'
 
 echo -e '\n\nSetup is complete\n\n'
