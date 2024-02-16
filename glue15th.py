47f3cb3b9ca7360bc8eb8a3e030f6749d3bcfa44
shubha262001 <90273666+shubha262001@users.noreply.github.com>
2024-02-16 14:37:36 +0530

    Update glue15th.py

--- glue15th.py
+++ glue15th.py
@@ -1,3 +1,76 @@
+import sys
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from pyspark.sql.functions import split, col, when, udf
+from pyspark.sql.types import FloatType, StringType, IntegerType, DoubleType
+from awsglue.job import Job
+
+# Create a GlueContext
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+job.init(args['JOB_NAME'], args)
+
+# Load the data into a DataFrame
+df = spark.read.parquet("s3://amazonsales-capstone-sk/cleanedfiles/")
+
+# Extract brand name from the first word of the product_name
+df = df.withColumn("brand_name", split(col("product_name"), " ")[0])
+
+# Convert discount_percentage to double and remove % symbol
+df = df.withColumn("discount_percentage(%)", col("discount_percentage").substr(1, 2).cast(DoubleType()))
+
+# Convert rating to double and handle cases where it is empty or not a valid number
+def parse_rating(rating):
+    try:
+        return float(rating)
+    except ValueError:
+        return None
+
+parse_rating_udf = udf(parse_rating, DoubleType())
+
+df = df.withColumn("rating_value", parse_rating_udf(col("rating")))
+
+# Calculate above_4_rating, 3to4_rating, and bad_review_percentage
+df = df.withColumn("above_4_rating", when(col("rating_value") > 4, 1).otherwise(0)) \
+    .withColumn("3to4_rating", when((col("rating_value") >= 3) & (col("rating_value") <= 4), 1).otherwise(0)) \
+    .withColumn("bad_review", when((col("above_4_rating") + col("3to4_rating")) == 0, 1).otherwise(0)) \
+    .withColumn("bad_review_percentage", (1 - (col("above_4_rating") + col("3to4_rating")))*100)
+
+# Calculate top performers
+from pyspark.sql.functions import desc
+top_performers_list = df.groupBy("product_id").count().orderBy(desc("count")).limit(10).select("product_id").collect()
+top_performers_list = [row.product_id for row in top_performers_list]
+df = df.withColumn("top_performer", col("product_id").isin(top_performers_list).cast(IntegerType()))
+
+# Drop unnecessary columns
+drop_columns = ['user_name', 'review_id', 'review_title', 'review_content', 'img_link', 'product_link', 'about_product']
+df_final = df.drop(*drop_columns)
+
+# Write the transformed data back to S3
+df_final.write.mode("overwrite").parquet("s3://amazonsales-capstone-sk/transformed/")
+
+job.commit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=======================================
 Error Category: UNSUPPORTED_OPERATION_ERROR; An error occurred while calling o206.parquet. org.apache.spark.sql.execution.datasources.SchemaColumnConvertNotSupportedException
 
 ------------------------------------------------------
